a19cc380f342d6365f05c8bd6cd40490b9401724
SAUL <54ulxd@gmail.com>
2019-09-19 22:51:29 -0500

    asdaSD

--- practicaSensorColor/practicaSensorColor.ino
+++ practicaSensorColor/practicaSensorColor.ino
@@ -1,76 +1,68 @@
-2#include <Servo.h>
-
-
-
-Servo myServo;
-
-const int s0 = 1;  
-const int s1 = 2;  
-const int s2 = 3;  
-const int s3 = 4;  
-const int out = 5;    
-int rojo = 0;  
-int verde = 0;  
-int azul = 0;
-
-bool r=false, g=false, b=false;
-
-void abrir()
-{
-  
-}
-
- void cerrar()
- {
-  
- }
-
-void color()  
-{    
-  digitalWrite(s2, LOW);  
-  digitalWrite(s3, LOW);   
-  rojo = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);  
-  digitalWrite(s3, HIGH);   
-  azul = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);  
-  digitalWrite(s2, HIGH);    
-  verde = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH); 
-  
-  if (rojo < azul && verde > azul && rojo < 35){  
-    r=true;
-    abrir();
-    Serial.println("   Rojo");    
-  }else if (azul < rojo && azul < verde && verde < rojo){  
-    b=true;
-    cerrar();
-    Serial.println("   Azul");        
-  }else{
-    
-    Serial.print(" azul  ");  
-    Serial.print(azul);
-     
-    Serial.print(" rojo  ");  
-    Serial.print(rojo); 
-     
-    Serial.print(" verde  ");  
-    Serial.print(verde);
-    Serial.println(" ");
-  }
-  delay(900);  
-}
-
-
+//
+// Cableado de TCS3200 a Arduino
+//
+#define S0 8
+#define S1 9
+#define S2 12
+#define S3 11
+#define salidaSensor 10
+ 
+// Para guardar las frecuencias de los fotodiodos
+int frecuenciaRojo = 0;
+int frecuenciaVerde = 0;
+int frecuenciaAzul = 0;
+ 
 void setup() {
-  // put your setup code here, to run once:
+  // Definiendo las Salidas
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+  
+  // Definiendo salidaSensor como entrada
+  pinMode(salidaSensor, INPUT);
+  
+  // Definiendo la escala de frecuencia a 20%
+  digitalWrite(S0,HIGH);
+  digitalWrite(S1,LOW);
+  
+   // Iniciar la comunicacion serie 
   Serial.begin(9600);
-  myServo.attach(9);
-   
-myServo.write(90);
-   delay(3000);
-
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-  color();
-
+  // Definiendo la lectura de los fotodiodos con filtro rojo
+  digitalWrite(S2,LOW);
+  digitalWrite(S3,LOW);
+  
+  // Leyendo la frecuencia de salida del sensor
+  frecuenciaRojo = pulseIn(salidaSensor, LOW);
+  
+  // Mostrando por serie el valor para el rojo (R = Red)
+  Serial.print("R = ");
+  Serial.print(frecuenciaRojo);
+  delay(100);
+  
+  // Definiendo la lectura de los fotodiodos con filtro verde
+  digitalWrite(S2,HIGH);
+  digitalWrite(S3,HIGH);
+  
+  // Leyendo la frecuencia de salida del sensor
+  frecuenciaVerde = pulseIn(salidaSensor, LOW);
+  
+  // Mostrando por serie el valor para el verde (G = Green)
+  Serial.print(" G = ");
+  Serial.print(frecuenciaVerde);
+  delay(100);
+ 
+  // Definiendo la lectura de los fotodiodos con filtro azul
+  digitalWrite(S2,LOW);
+  digitalWrite(S3,HIGH);
+  
+  // Leyendo la frecuencia de salida del sensor
+  frecuenciaAzul = pulseIn(salidaSensor, LOW);
+  
+  // Mostrando por serie el valor para el azul (B = Blue)
+  Serial.print(" B = ");
+  Serial.println(frecuenciaAzul);
+  delay(100);
 }
